7f71905264f98f24023ff1293e466b04bb58a335
thedirtypaw <tudor.nicolau@gmail.com>
2026-06-22 20:50:46 +0300

    mobile full res

--- app/mobile/page.tsx
+++ app/mobile/page.tsx
@@ -2,11 +2,11 @@ import Image from 'next/image';
 
 export default function MobilePage() {
     return (
-      <div className="w-full h-screen flex items-center justify-center">
-        <img 
-          src="/landing.webp" 
+      <div className="w-full min-h-screen">
+        <img
+          src="/landing.webp"
           alt="Mobile Layout"
-          className="w-full h-full object-contain"
+          className="w-full h-auto"
         />
       </div>
     );
