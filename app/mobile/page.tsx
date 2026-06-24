0ba12a8f430fe27d6ee35eebc10fa4cd03e06f77
thedirtypaw <tudor.nicolau@gmail.com>
2026-06-24 04:28:56 +0300

    mobile draft11

--- app/mobile/page.tsx
+++ app/mobile/page.tsx
@@ -37,13 +37,13 @@ export default function MobilePage() {
       <section className="relative h-[100svh] overflow-hidden">
         <FlowField className="absolute inset-0 pointer-events-none" />
         <div className={`relative z-10 ${col} h-full flex flex-col justify-center text-white mix-blend-difference`}>
-          <div className={`text-8xl sm:text-8xl text-pink-700 font-black  ${gabarito.className}`}>
+          <div className={`text-8xl sm:text-4xl text-pink-700 font-black  ${gabarito.className}`}>
             Therapeutic Nutrition
           </div>
-          <h2 className={`text-6xl mt-4 text-[#a23865] ${basic.className}`}>
+          <h2 className={`text-6xl sm:text-3xl mt-4 text-[#a23865] ${basic.className}`}>
             We make superior quality plant-based therapeutic nutrition products for brain, metabolic and gastrointestinal health
           </h2>
-          <p className={`mt-6 text-3xl text-[#d87294] ${basic.className}`}>
+          <p className={`mt-6 text-3xl sm:text-2xl text-[#d87294] ${basic.className}`}>
             With our zero-waste patentable process and whole-matrix formulation
             From organic yellow peas grown at nature-friendly farms, for healthcare practitioners and active patients
           </p>
